b4857d5dfe5ee4b993ee0bff295e4513dc33ebff
git-alexis <alex59.billemont@gmail.com>
2023-11-23 16:31:12 +0100

    ajout descriptions et implémentation du legnth dans la boucle dans _printf

--- get_modulo_fonctions.c
+++ get_modulo_fonctions.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include "stdlib.h"
-
+/**
+*get_fonctions - function pointer between the character and the good function
+*@letter: pointer contain letter associate to function
+*Return: NULL
+*/
 int (*get_fonctions(char *letter))(va_list)
 {
 	specifier_t fonction_pointeur[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_modulo},
-		{"i", print_integer},
 		{"d", print_decimal},
+		{"i", print_integer},
 		{NULL, NULL}
 	};
 	int index = 0;
@@ -17,7 +21,7 @@ int (*get_fonctions(char *letter))(va_list)
 	{
 		if (fonction_pointeur[index].specifier[0] == letter[0])
 		{
-				return (fonction_pointeur[index].fonction);
+			return (fonction_pointeur[index].fonction);
 		}
 		index++;
 	}
--- print_functions.c
+++ print_functions.c
@@ -1,11 +1,19 @@
 #include "main.h"
-
+/**
+*print_char - print a character
+*@args: argument
+*Return: 1
+*/
 int print_char(va_list args)
 {
 	_putchar(va_arg(args, int));
 	return (1);
 }
-
+/**
+*print_string - print a string
+*@args: argument
+*Return: index
+*/
 int print_string(va_list args)
 {
 	char *string = va_arg(args, char *);
@@ -17,14 +25,57 @@ int print_string(va_list args)
 	}
 	return (index);
 }
-
+/**
+*print_modulo- print a percent
+*@args: argument
+*Return: 1
+*/
 int print_modulo(va_list args)
 {
 	(void)args;
 	_putchar('%');
 	return (1);
 }
+/**
+*print_decimal - print a decimal
+*@args: argument
+*Return: length
+*/
+int print_decimal(va_list args)
+{
+	int decimal = (va_arg(args, int));
+	int digit = 0;
+	int reverse = 0;
+	int length = 0;
 
+	if (decimal < 0)
+	{
+		_putchar('-');
+		length += 1;
+		decimal *= -1;
+	}
+
+	while (decimal > 0)
+	{
+		digit = decimal % 10;
+		reverse = reverse * 10 + digit;
+		decimal = decimal / 10;
+	}
+
+	while (reverse > 0)
+	{
+		digit = reverse % 10;
+		_putchar(digit + '0');
+		reverse /= 10;
+		length++;
+	}
+	return (length);
+}
+/**
+*print_integer - print a integer
+*@args: argument
+*Return: length
+*/
 int print_integer(va_list args)
 {
 	int integer = (va_arg(args, int));
@@ -36,16 +87,16 @@ int print_integer(va_list args)
 	{
 		_putchar('-');
 		length += 1;
-		integer = -integer;
+		integer *= -1;
 	}
-	
+
 	while (integer > 0)
 	{
 		digit = integer % 10;
 		reverse = reverse * 10 + digit;
 		integer = integer / 10;
 	}
-	
+
 	while (reverse > 0)
 	{
 		digit = reverse % 10;
@@ -55,34 +106,3 @@ int print_integer(va_list args)
 	}
 	return (length);
 }
-
-int print_decimal(va_list args)
-{
-	int decimal = (va_arg(args, int));
-	int digit = 0;
-	int reverse = 0;
-	int length = 0;
-
-	if (decimal < 0)
-	{
-		_putchar('-');
-		decimal *= -1;
-	}
-	
-	while (decimal > 0)
-	{
-		digit = decimal % 10;
-		reverse = reverse * 10 + digit;
-		decimal = decimal / 10;
-	}
-	
-	while (reverse > 0)
-	{
-		digit = reverse % 10;
-		_putchar(digit + '0');
-		reverse /= 10;
-		length++;
-	}
-	return (length);
-}
-
